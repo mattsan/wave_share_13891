f069066ee23901eaeace470c70a96f0a533d08a1
Eiji MATSUMOTO <e.mattsan@gmail.com>
2024-01-24 22:32:04 +0900

    delegate some functions

--- lib/wave_share13891.ex
+++ lib/wave_share13891.ex
@@ -21,6 +21,9 @@ defmodule WaveShare13891 do
 
   defdelegate register(key_or_keys), to: WaveShare13891.KeyEvent
   defdelegate register(key_or_keys, subscriber), to: WaveShare13891.KeyEvent
+  defdelegate set_backlight(condition), to: WaveShare13891.LCD
+  defdelegate set_window(x_start, y_start, x_end, y_end), to: WaveShare13891.LCD
+  defdelegate write_data(data), to: WaveShare13891.LCD
 
   @impl true
   def init(_opts) do
