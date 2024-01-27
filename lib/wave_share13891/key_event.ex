2f5037b6cf6e5b0dd4e655bbac54ca079d60e7b0
Eiji MATSUMOTO <e.mattsan@gmail.com>
2024-01-27 20:09:51 +0900

    cosmetic

--- lib/wave_share13891/key_event.ex
+++ lib/wave_share13891/key_event.ex
@@ -8,15 +8,14 @@ defmodule WaveShare13891.KeyEvent do
   alias Circuits.GPIO
 
   @name __MODULE__
-  # input pins
-  @pin_in_up 6
-  @pin_in_down 19
-  @pin_in_left 5
-  @pin_in_right 26
-  @pin_in_press 13
-  @pin_in_key1 21
-  @pin_in_key2 20
-  @pin_in_key3 16
+  @gpio_up 6
+  @gpio_down 19
+  @gpio_left 5
+  @gpio_right 26
+  @gpio_press 13
+  @gpio_key1 21
+  @gpio_key2 20
+  @gpio_key3 16
 
   @type key() :: :up | :down | :left | :right | :press | :key1 | :key2 | :key3
 
@@ -63,20 +62,36 @@ defmodule WaveShare13891.KeyEvent do
 
   @impl true
   def init(_opts) do
-    state = initialize_input()
+    state = initialize_gpio()
 
     {:ok, state}
   end
 
-  defp initialize_input do
-    {:ok, up} = GPIO.open(@pin_in_up, :input, pull_mode: :pullup)
-    {:ok, down} = GPIO.open(@pin_in_down, :input, pull_mode: :pullup)
-    {:ok, left} = GPIO.open(@pin_in_left, :input, pull_mode: :pullup)
-    {:ok, right} = GPIO.open(@pin_in_right, :input, pull_mode: :pullup)
-    {:ok, press} = GPIO.open(@pin_in_press, :input, pull_mode: :pullup)
-    {:ok, key1} = GPIO.open(@pin_in_key1, :input, pull_mode: :pullup)
-    {:ok, key2} = GPIO.open(@pin_in_key2, :input, pull_mode: :pullup)
-    {:ok, key3} = GPIO.open(@pin_in_key3, :input, pull_mode: :pullup)
+  @impl true
+  def handle_cast({:register, key, subscriber}, state) do
+    unless registered?(key, subscriber) do
+      {:ok, _pid} = Registry.register(Registry.WaveShare13891, key, subscriber)
+    end
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:circuits_gpio, pin_number, timestamp, value}, state) do
+    broadcast(pin_number_to_key(pin_number), timestamp, kind(value))
+
+    {:noreply, state}
+  end
+
+  defp initialize_gpio do
+    {:ok, up} = GPIO.open(@gpio_up, :input, pull_mode: :pullup)
+    {:ok, down} = GPIO.open(@gpio_down, :input, pull_mode: :pullup)
+    {:ok, left} = GPIO.open(@gpio_left, :input, pull_mode: :pullup)
+    {:ok, right} = GPIO.open(@gpio_right, :input, pull_mode: :pullup)
+    {:ok, press} = GPIO.open(@gpio_press, :input, pull_mode: :pullup)
+    {:ok, key1} = GPIO.open(@gpio_key1, :input, pull_mode: :pullup)
+    {:ok, key2} = GPIO.open(@gpio_key2, :input, pull_mode: :pullup)
+    {:ok, key3} = GPIO.open(@gpio_key3, :input, pull_mode: :pullup)
 
     GPIO.set_interrupts(up, :both)
     GPIO.set_interrupts(down, :both)
@@ -99,52 +114,35 @@ defmodule WaveShare13891.KeyEvent do
     }
   end
 
-  @impl true
-  def handle_cast({:register, key, subscriber}, state) do
-    unless registered?(key, subscriber) do
-      {:ok, _pid} = Registry.register(Registry.WaveShare13891, key, subscriber)
-    end
-
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_info({:circuits_gpio, pin_number, timestamp, value}, state) do
-    key = pin_number_to_key(pin_number)
-
-    kind =
-      case value do
-        0 -> :pressed
-        1 -> :released
-      end
-
-    Registry.dispatch(Registry.WaveShare13891, key, fn entries ->
-      for {_pid, subscriber} <- entries do
-        send(subscriber, {:key_event, key, timestamp, kind})
-      end
-    end)
-
-    {:noreply, state}
-  end
-
-  defp pin_number_to_key(pin_number) do
-    case pin_number do
-      @pin_in_up -> :up
-      @pin_in_down -> :down
-      @pin_in_left -> :left
-      @pin_in_right -> :right
-      @pin_in_press -> :press
-      @pin_in_key1 -> :key1
-      @pin_in_key2 -> :key2
-      @pin_in_key3 -> :key3
-    end
-  end
-
   defp registered?(key, subscriber) do
     Registry.lookup(Registry.WaveShare13891, key)
     |> Enum.any?(fn
       {_, ^subscriber} -> true
       _ -> false
+    end)
+  end
+
+  defp pin_number_to_key(pin_number) do
+    case pin_number do
+      @gpio_up -> :up
+      @gpio_down -> :down
+      @gpio_left -> :left
+      @gpio_right -> :right
+      @gpio_press -> :press
+      @gpio_key1 -> :key1
+      @gpio_key2 -> :key2
+      @gpio_key3 -> :key3
+    end
+  end
+
+  defp kind(0), do: :pressed
+  defp kind(1), do: :released
+
+  defp broadcast(key, timestamp, kind) do
+    Registry.dispatch(Registry.WaveShare13891, key, fn entries ->
+      for {_pid, subscriber} <- entries do
+        send(subscriber, {:key_event, key, timestamp, kind})
+      end
     end)
   end
 end
